c0693ddede4f58a1611c98c31bd1565ebd913880
Patriciadesu <118905032+Patriciadesu@users.noreply.github.com>
2025-04-16 17:21:59 +0700

    update

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,8 +1,15 @@
 using UnityEngine;
-
+using NaughtyAttributes;
 public class Coin : MonoBehaviour
 {
     [SerializeField] private int price = 5;
+    public float rotateSpeed;
+
+    private void Update()
+    {
+        Debug.Log("Rotation is " + transform.rotation);
+        Debug.Log("Euler Angle is " + transform.rotation.eulerAngles);
+    }
 
     void PrintCurrentMoney(int currentMoney)
     {
@@ -23,6 +30,26 @@ public class Coin : MonoBehaviour
     {
         GameManager.Instance.Money = price;
         Destroy(gameObject);
+    }
+    [Button("Rotate Quaternion.Identity")]
+    public void Quaternion_Identity()
+    {
+        transform.rotation = Quaternion.identity;
+    }
+    [Button("Rotate 0 Degree")]
+    public void _0Degree()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+    [Button("Rotate 90 Degree")]
+    public void _90Degree()
+    {
+        transform.rotation = Quaternion.Euler(0,90,0);
+    }
+    [Button("Rotate 180 Degree")]
+    public void _180Degree()
+    {
+        transform.rotation = Quaternion.Euler(0, 180, 0);
     }
 
 }
